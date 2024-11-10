1b2082a34c2b933142a3ec242ff95f726e0c4c5d
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-11-10 20:16:16 -0300

    feat matriz adyacencia

--- main.c
+++ main.c
@@ -1,2 +1,48 @@
-hola
-dou endeaf
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[]){
+	FILE *file = fopen("grafo.txt", "r");
+	char linea[256];
+	char *inicio_tok;
+	char *tok;
+	int n;
+
+	if(fgets(linea, sizeof(linea), file) != NULL){
+		linea[strcspn(linea, "\n")] = '\0';
+		n = atoi(linea);
+	}
+	
+	int matriz_ad[n][n];
+	int fila = 0;
+
+	for(int x = 0; x < n; x++){
+		for(int y = 0; y < n; y++){
+			matriz_ad[x][y] = 0;
+		}
+	}
+
+	while(fgets(linea, sizeof(linea), file) != NULL){
+		inicio_tok = strstr(linea, " ");
+		tok = strtok(inicio_tok, ", ");
+	    while (tok != NULL) {
+	    	matriz_ad[fila][atoi(tok) - 1] = 1;
+	        tok = strtok(NULL, ", "); 
+	    }	
+		fila += 1;
+	}
+
+	for(int x = 0; x < n; x++){
+		for(int y = 0; y < n; y++){
+			printf("%d ", matriz_ad[x][y]);
+		}
+		printf("\n");
+	}
+
+
+
+}
+
+
