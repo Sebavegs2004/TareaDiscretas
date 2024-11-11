7db85aad62a51c2309a43d7c44f1e3e6359f4a8f
Lunara02 <cristobagonzalez2023@inf.udec.cl>
2024-11-11 09:22:07 -0300

    feat: Esconexo y dfs

--- main.c
+++ main.c
@@ -1,42 +1,90 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-int main(int argc, char const *argv[]){
-	FILE *file = fopen("grafo.txt", "r");
-	char linea[256];
-	int n;
+int **grafo;
+int *visitado;
+int V;
 
-	if(fgets(linea, sizeof(linea), file) != NULL){
-		linea[strcspn(linea, "\n")] = '\0';
-		n = atoi(linea);
-	}
-	
-	int matriz_ad[n][n];
-	int fila = 0;
-
-	for(int x = 0; x < n; x++){
-		for(int y = 0; y < n; y++){
-			matriz_ad[x][y] = 0;
-		}
-	}
-
-	while(fgets(linea, sizeof(linea), file) != NULL){
-		for(int x = 2; x < strlen(linea); x++){
-			if(isdigit(linea[x]) && (atoi(&linea[x]) <= n)){
-				matriz_ad[fila][atoi(&linea[x]) - 1] = 1;
-			}
-		}
-		fila += 1;
-	}
-
-	for(int x = 0; x < n; x++){
-		for(int y = 0; y < n; y++){
-			printf("%d ", matriz_ad[x][y]);
-		}
-		printf("\n");
-	}
+void DFS(int v) {
+    visitado[v] = 1;
+    for (int i = 0; i < V; i++) {
+        if (grafo[v][i] == 1 && !visitado[i]) {
+            DFS(i);
+        }
+    }
 }
 
+int esConexo() {
+    for (int i = 0; i < V; i++) {
+        visitado[i] = 0;
+    }
+    DFS(0);
 
+    for (int i = 0; i < V; i++) {
+        if (!visitado[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    FILE *file = fopen("grafo.txt", "r");
+    if (file == NULL) {
+        printf("Error al abrir el archivo.\n");
+        return 1;
+    }
+
+    char linea[256];
+    if (fgets(linea, sizeof(linea), file) != NULL) {
+        linea[strcspn(linea, "\n")] = '\0';
+        V = atoi(linea);
+    }
+
+    grafo = (int **)malloc(V * sizeof(int *));
+    for (int i = 0; i < V; i++) {
+        grafo[i] = (int *)malloc(V * sizeof(int));
+    }
+    visitado = (int *)malloc(V * sizeof(int));
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            grafo[i][j] = 0;
+        }
+    }
+
+    int fila = 0;
+    while (fgets(linea, sizeof(linea), file) != NULL && fila < V) {
+        for (int x = 2; x < strlen(linea); x++) {
+            if (isdigit(linea[x]) && (atoi(&linea[x]) <= V)) {
+                grafo[fila][atoi(&linea[x]) - 1] = 1;
+            }
+        }
+        fila += 1;
+    }
+    fclose(file);
+
+    printf("Matriz de adyacencia:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            printf("%d ", grafo[i][j]);
+        }
+        printf("\n");
+    }
+
+    if (esConexo()) {
+        printf("El grafo es conexo\n");
+    } else {
+        printf("El grafo no es conexo\n");
+    }
+
+    for (int i = 0; i < V; i++) {
+        free(grafo[i]);
+    }
+    free(grafo);
+    free(visitado);
+
+    return 0;
+}
