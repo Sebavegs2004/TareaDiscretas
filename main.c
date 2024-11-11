f916aa5b14683220ad16c7c3c03f68ba0a9503c5
Lunara02 <cristobagonzalez2023@inf.udec.cl>
2024-11-11 08:34:16 -0300

    Revert "feat: dfs exconexo"
    This reverts commit 80cec5a178a7b1a0e29bb05b75a70cf835695f99.

--- main.c
+++ main.c
@@ -2,29 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdbool.h>
-
-#define MAX_NODES 20
-
-void dfs(int nodo, bool visited[], int matriz_ad[][MAX_NODES]) {
-    visited[nodo] = true;
-    for (int i = 0; i < MAX_NODES; i++) {
-        if (matriz_ad[nodo][i] == 1 && !visited[i]) {
-            dfs(i, visited, matriz_ad);
-        }
-    }
-}
-
-bool es_conexo(int matriz_ad[][MAX_NODES]) {
-    bool visited[MAX_NODES] = {false};
-    dfs(0, visited, matriz_ad);
-    for (int i = 0; i < MAX_NODES; i++) {
-        if (!visited[i]) {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main(int argc, char const *argv[]){
 	FILE *file = fopen("grafo.txt", "r");
@@ -60,14 +37,6 @@ int main(int argc, char const *argv[]){
 		}
 		printf("\n");
 	}
-
-	if (es_conexo(matriz_ad, n)) {
-        printf("El grafo es conexo.\n");
-    } else {
-        printf("El grafo no es conexo.\n");
-    }
-
-    return 0;
 }
 
 
